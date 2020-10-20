59af26ee4f7b9da352951c82ccf1a4e3fb8d2901
Jere Aho <jere.aho@tuni.fi>
2020-10-20 20:09:35 +0300

    fixed errors relating to tampere -> city

--- Game/creategame.cc
+++ Game/creategame.cc
@@ -1,6 +1,6 @@
 #include "creategame.hh"
 
-#include "tampere.hh"
+#include "city.hh"
 
 #include <memory>
 #include <QImage>
@@ -8,11 +8,11 @@
 const QString DEFAULT_SMALLBACKGROUND_FILE = ":/offlinedata/offlinedata/kartta_pieni_500x500.png";
 const QString DEFAULT_BIGBACKGROUND_FILE = ":/offlinedata/offlinedata/kartta_iso_1095x592.png";
 
-using StudentSide::Tampere;
+using Interface::City;
 
 std::shared_ptr<Interface::ICity> Interface::createGame()
 {
-    std::shared_ptr<Tampere> city = std::make_shared<Tampere>(Tampere());
+    std::shared_ptr<City> city = std::make_shared<City>(City());
     QImage smallBackground = QImage(DEFAULT_SMALLBACKGROUND_FILE, "png");
     QImage bigBackground = QImage(DEFAULT_BIGBACKGROUND_FILE, "png");
     city->setBackground(smallBackground, bigBackground);
