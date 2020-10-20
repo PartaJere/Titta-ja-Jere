d8de6462fe313ea3a5a7c9d2d565b4fc915c8daf
Titta Kemppi <kemppi@linux-desktop5.tuni.fi>
2020-10-20 12:30:41 +0300

    Change class name

--- Game/city.cc
+++ Game/city.cc
@@ -4,63 +4,63 @@
 namespace Interface
 {
 
-Tampere::Tampere() : Interface::ICity()
+City::City() : Interface::ICity()
 {
 
 }
 
-void Tampere::setBackground(QImage &basicbackground, QImage &bigbackground)
+void City::setBackground(QImage &basicbackground, QImage &bigbackground)
 {
 
     return;
 }
 
-void Tampere::setClock(QTime clock)
+void City::setClock(QTime clock)
 {
     return;
 }
 
-void Tampere::addStop(std::shared_ptr<IStop> stop)
+void City::addStop(std::shared_ptr<IStop> stop)
 {
     return;
 }
 
-void Tampere::startGame()
+void City::startGame()
 {
     return;
 }
 
-void Tampere::addActor(std::shared_ptr<IActor> newactor)
+void City::addActor(std::shared_ptr<IActor> newactor)
 {
     return;
 }
 
-void Tampere::removeActor(std::shared_ptr<IActor> actor)
+void City::removeActor(std::shared_ptr<IActor> actor)
 {
     return;
 }
 
-void Tampere::actorDestroyed(std::shared_ptr<IActor> actor)
+void City::actorDestroyed(std::shared_ptr<IActor> actor)
 {
     return;
 }
 
-bool Tampere::findActor(std::shared_ptr<IActor> actor) const
+bool City::findActor(std::shared_ptr<IActor> actor) const
 {
     return false;
 }
 
-void Tampere::actorMoved(std::shared_ptr<IActor> actor)
+void City::actorMoved(std::shared_ptr<IActor> actor)
 {
     return;
 }
 
-std::vector<std::shared_ptr<IActor> > Tampere::getNearbyActors(Location loc) const
+std::vector<std::shared_ptr<IActor> > City::getNearbyActors(Location loc) const
 {
 
 }
 
-bool Tampere::isGameOver() const
+bool City::isGameOver() const
 {
     return false;
 };
