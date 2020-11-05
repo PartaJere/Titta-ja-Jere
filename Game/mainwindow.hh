d97bf7121836aeb6a18eeef9d3b215a8d2f2f7d2
Jere Aho <jere.aho@tuni.fi>
2020-11-06 00:05:28 +0200

    add a listener to key presses and releases

--- Game/mainwindow.hh
+++ Game/mainwindow.hh
@@ -12,6 +12,7 @@
 #include <iostream>
 #include <memory>
 #include <QVector>
+#include <QKeyEvent>
 #include <map>
 
 namespace Ui {
@@ -42,9 +43,13 @@ public:
 
 signals:
     void gameStarted();
+    void keyPressed(int key);
+    void keyReleased(int key);
 
 private slots:
     void on_startButton_clicked();
+    void keyPressEvent( QKeyEvent* event );
+    void keyReleaseEvent( QKeyEvent* event);
 
 private:
     Ui::MainWindow *ui;
