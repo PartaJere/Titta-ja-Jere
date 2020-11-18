289e47940683413d1fd29866cadbf64ed5e08d7f
Jere Aho <jere.aho@tuni.fi>
2020-11-18 17:09:54 +0200

    add comments

--- Game/player.hh
+++ Game/player.hh
@@ -15,11 +15,49 @@ namespace Game {
         //Actor interface
         std::string getName() const;
 
+        /**
+         * @brief giveLocation returns the location of the actor.
+         * @pre -
+         * @return Actors location.
+         * @post Exception guarantee: strong.
+         * @exception GameError - actor wasn't given a location.
+         */
         Interface::Location giveLocation() const;
+        /**
+         * @brief move-method moves the actor to given location.
+         * @param loc Actors new location.
+         * @pre -
+         * @post Actors location is sij. Excaption guarantee: strong.
+         * @exception GameError Location is not possible.
+         */
         void move(Interface::Location loc);
+        /**
+         * @brief remove marks the actor as removed.
+         * @pre Actor is not removed already.
+         * @post Actor is removed, after this isRemoved() returns `true`. Exception guarantee: basic.
+         */
         void remove();
+        /**
+         * @brief isRemoved tells if the actor is removed ingame.
+         * @pre -
+         * @return `true`, if actor is removed ingame, otherwise `false`.
+         * @post Exception guarantee: nothrow.
+         */
         bool isRemoved() const;
+        /**
+         * @brief getSpeed tells the speed of the actor
+         * @pre -
+         * @return int, pixels per move
+         * @post Exception guarantee: nothrow.
+         */
         int getSpeed();
+        /**
+         * @brief changeSpeed is used to change the speed (pixels per move) of the player
+         * @pre -
+         * @post speed is between 1-50 pix/move
+         * @return 'true' if speed is acceptable, 'false' if not
+         * @post Exception guarantee: basic
+         */
         bool changeSpeed(int newSpeed);
 
     private:
