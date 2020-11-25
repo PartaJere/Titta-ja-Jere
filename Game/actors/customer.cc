4c0b7b9d8ae82eefff0e037c6aae4506748bb02a
Jere Aho <jere.aho@tuni.fi>
2020-11-25 13:34:15 +0200

    add customer object

---
+++ Game/actors/customer.cc
@@ -0,0 +1,29 @@
+#include "customer.hh"
+
+Customer::Customer() : location_(),
+                        removed_(false)
+{
+    int x = 100;
+    int y = 200;
+    location_.setXY(x, y);
+}
+
+Interface::Location Customer::giveLocation() const
+{
+    return location_;
+}
+
+void Customer::move(Interface::Location loc)
+{
+    location_ = loc;
+}
+
+void Customer::remove()
+{
+    removed_ = true;
+}
+
+bool Customer::isRemoved() const
+{
+    return removed_;
+}
