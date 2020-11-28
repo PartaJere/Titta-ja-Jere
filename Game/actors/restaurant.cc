c79b2fa43c5ed9d9a02d68f92810ea363276b26d
Titta <titta.kemppi@tuni.fi>
2020-11-28 12:28:21 +0200

    Add Q_ASSERTs

--- Game/actors/restaurant.cc
+++ Game/actors/restaurant.cc
@@ -13,12 +13,23 @@ Restaurant::Restaurant(Interface::Location location, int maxFood) :  location_(l
 
     Interface::Location Restaurant::giveLocation() const
     {
-        return location_;
+
+            return location_;
+
     }
 
     void Restaurant::move(Interface::Location loc)
     {
+        try {
         location_ = loc;
+
+        }  catch (...) {
+
+        Interface::GameError badloc("Location is not possible.");
+        badloc.what();
+        throw badloc;
+
+    }
     }
 
     void Restaurant::remove()
@@ -33,6 +44,8 @@ Restaurant::Restaurant(Interface::Location location, int maxFood) :  location_(l
 
     void Restaurant::setMaxFood(int maxFood)
     {
+        Q_ASSERT(maxFood >= 0);
+        Q_ASSERT(maxFood_ >= 0);
         maxFood_ = maxFood;
     }
 
@@ -53,14 +66,20 @@ Restaurant::Restaurant(Interface::Location location, int maxFood) :  location_(l
 
     void Restaurant::removeFood(int amount)
     {
+        Q_ASSERT(amount >= 0);
         if(foodReady_ > amount){
             foodReady_ -= amount;
         }else{
             foodReady_ = 0;
         }
 
-
     }
+
+    void Restaurant::reset()
+    {
+        foodReady_ = maxFood_/2;
+    }
+
 
 
     void Restaurant::addFood()
@@ -70,10 +89,6 @@ Restaurant::Restaurant(Interface::Location location, int maxFood) :  location_(l
         }
     }
 
-    void Restaurant::reset()
-    {
-        foodReady_ = maxFood_/2;
-    }
 
 }
 
