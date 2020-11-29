c16976bbc587e005bbe4c8a77f31f56298d464d8
Titta Kemppi <kemppi@linux-desktop9.tuni.fi>
2020-11-29 11:37:18 +0200

    Add comments

--- Game/windows/startwindow.hh
+++ Game/windows/startwindow.hh
@@ -16,16 +16,38 @@ class StartWindow : public QDialog
     Q_OBJECT
 
 public:
+    /**
+     * @brief StartWindow sets up Ui
+     * @param parent QDialog
+     */
     explicit StartWindow(QWidget *parent = nullptr);
+
     ~StartWindow();
 
 signals:
+
+    /**
+     * @brief setPlayerName is a signal that carries player's name to be set somewhere
+     * @param name is the name player has inserted for themselved
+     */
     void setPlayerName(std::string name);
+
+    /**
+     * @brief setDifficulty is a signal that carries the game difficulty to be set
+     * @param difficulty is the difficulty the player has chosen for their game
+     */
     void setDifficulty(std::string difficulty);
 
 private slots:
+
+    /**
+     * @brief on_buttonBox_accepted gets information from startwindow and sends signals out into the world
+     */
     void on_buttonBox_accepted();
 
+    /**
+     * @brief on_buttonBox_rejected closes the startwindow
+     */
     void on_buttonBox_rejected();
 
 private:
